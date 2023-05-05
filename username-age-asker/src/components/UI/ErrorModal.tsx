e6a75b1f1e0080a00f4e9f3b03a1f1c1e1845713
dhruva1995 <helloworldiamnew@gmail.com>
2023-05-05 09:48:27 +0530

    Moved the backdrop and overlay to be the direct child of body using ReactPortals

--- username-age-asker/src/components/UI/ErrorModal.tsx
+++ username-age-asker/src/components/UI/ErrorModal.tsx
@@ -1,3 +1,4 @@
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
@@ -6,9 +7,16 @@ const ErrorModal: React.FC<{
   message: string;
   onCancel: () => void;
 }> = (props) => {
-  return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onCancel}></div>
+  const BackDrop: React.FC<{ onCancel: () => void }> = (props) => {
+    return <div className={styles.backdrop} onClick={props.onCancel}></div>;
+  };
+
+  const Modal: React.FC<{
+    title: string;
+    message: string;
+    onCancel: () => void;
+  }> = (props) => {
+    return (
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
@@ -18,7 +26,27 @@ const ErrorModal: React.FC<{
           <Button onClick={props.onCancel}>Okay</Button>
         </footer>
       </Card>
-    </div>
+    );
+  };
+  const backDropRoot = document.getElementById("backdrop-root");
+  const overlayRoot = document.getElementById("overlay-root");
+  return (
+    <>
+      {backDropRoot &&
+        ReactDOM.createPortal(
+          <BackDrop onCancel={props.onCancel} />,
+          backDropRoot
+        )}
+      {overlayRoot &&
+        ReactDOM.createPortal(
+          <Modal
+            title={props.title}
+            message={props.message}
+            onCancel={props.onCancel}
+          />,
+          overlayRoot
+        )}
+    </>
   );
 };
 
